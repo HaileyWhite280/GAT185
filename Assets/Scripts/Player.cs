3952749d6a65c9d53f617b4b518986fa4b9aa5e1
Meliemau <hwhite@student.neumont.edu>
2022-01-10 10:57:36 -0700

    Did things in class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(0, 10)][Tooltip("Speed of Player")] public float speed = 5;
+    [SerializeField] AudioSource audioSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        transform.position += direction * speed * Time.deltaTime;
+        //transform.rotation *= Quaternion.Euler(25, 0, 0);
+        //transform.localScale = new Vector3(2, 2, 2);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            audioSource.Play();
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        GameObject go = GameObject.Find("Cube");
+        if(go)
+        {
+            go.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
